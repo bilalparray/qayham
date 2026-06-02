06ad66982ecbefcd52143204aea4c35e71f37314
Bilal Ahmad <dev.renosoftwares@gmail.com>
2026-06-02 13:21:12 +0530

    website url added in menu and settings with copy button and url opener

--- cloudnotes/flutter_bootstrap.js
+++ cloudnotes/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1475734766"
+    serviceWorkerVersion: "1629642744"
   }
 });
